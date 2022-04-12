1160afe7c920e2ff803f6275f921730f6e56ede6
Judirale13 <jramirezle@unal.edu.co>
2022-04-12 12:21:16 -0500

    Initial commit

---
+++ src/main/java/com/example/entrenate/EntrenateApplication.java
@@ -0,0 +1,13 @@
+package com.example.entrenate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrenateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntrenateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/entrenate/EntrenateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entrenate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrenateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
